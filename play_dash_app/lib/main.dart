36b874ee80ee81c9a7111428869d249012e99070
dactri <skyphantomhz@gmail.com>
2026-03-30 12:04:05 +0700

    fix: enforce hash routing for static web server compatibility

--- play_dash_app/lib/main.dart
+++ play_dash_app/lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'app/app.dart';
 
 void main() {
+  if (kIsWeb) {
+    setUrlStrategy(HashUrlStrategy());
+  }
+
   runApp(
     const ProviderScope(
       child: PlayDashApp(),
